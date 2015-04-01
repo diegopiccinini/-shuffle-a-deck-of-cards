d087be559b675b4f4a6c8fd9a78edaa511420996
Diego <diego@guiasrails.es>
2015-04-01 12:03:10 +0200

    updating lib files path in application config

--- config/application.rb
+++ config/application.rb
@@ -1,5 +1,5 @@
 require File.expand_path('../boot', __FILE__)
-Dir[File.join(File.dirname(__FILE__), '..','..','lib', '*.rb')].each {|file| require file }
+Dir[File.join(File.dirname(__FILE__), '..','lib', '*.rb')].each {|file| require file }
 require 'rails/all'
 
 # Require the gems listed in Gemfile, including any gems
